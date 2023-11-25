49979b0d4c66b5bc70e89e746346aa226d0ecb79
aion0721 <rudolf.aion@gmail.com>
2023-11-25 17:50:47 +0900

    add gross calculate

--- src/components/organisms/SalesManagementForm.tsx
+++ src/components/organisms/SalesManagementForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Button } from "@chakra-ui/react";
 import LabeledDropdown from "../molecules/LabeledDropdown";
 import LabeledInput from "../molecules/LabeledInput";
@@ -15,6 +15,15 @@ const SalesManagementForm: React.FC = () => {
     { value: "bbb", label: "option2" },
     { value: "ccc", label: "option3" },
   ];
+
+  const calculateGrossMargin = (cost: number, price: number) => {
+    return price - cost;
+  };
+
+  useEffect(() => {
+    const margin = calculateGrossMargin(costPrice, sellingPrice);
+    setGrossMargin(margin);
+  }, [costPrice, sellingPrice]);
   return (
     <Box as="form">
       <LabeledDropdown
@@ -27,13 +36,13 @@ const SalesManagementForm: React.FC = () => {
         label="cost"
         type="number"
         value={costPrice}
-        onChange={(e) => setCostPrice(e.target.valueAsNumber)}
+        onChange={(e) => setCostPrice(parseFloat(e.target.value) || 0)}
       />
       <LabeledInput
         label="price"
         type="number"
         value={sellingPrice}
-        onChange={(e) => setSellingPrice(e.target.valueAsNumber)}
+        onChange={(e) => setSellingPrice(parseFloat(e.target.value) || 0)}
       />
       <Box>Gross Margin: {grossMaqrgin}</Box>
       <Box>Margin Percentage: {marginPercentage}%</Box>
